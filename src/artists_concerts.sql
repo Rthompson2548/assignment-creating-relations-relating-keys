4c02cd1e205f9f5fed9a248181efa1100737ddc5
Reilly Thompson <thompson.reilly@yahoo.com>
2021-07-26 17:13:41 -0700

    first commit

---
+++ src/artists_concerts.sql
@@ -0,0 +1,24 @@
+-- SYNTAX:
+-- CREATE TABLE table_name (
+--    column_name data_type  column_constraint [ , ]
+-- )
+
+create table artists_concerts 
+(
+    artist_id 
+    integer 
+    references artists(artist_id),
+
+    concert_id 
+    integer 
+    references concerts(concert_id),
+   
+    scheduled_start_at 
+    time,
+
+    scheduled_end_at 
+    time,
+    
+    PRIMARY KEY (artist_id,concert_id)
+)
+
